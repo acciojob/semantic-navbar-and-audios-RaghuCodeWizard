97a6499b842f7955e64de3079a0e68fb5c1770ae
Raghavendra M <137168147+RaghuCodeWizard@users.noreply.github.com>
2023-08-25 18:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-//your code here
+cy.visit('your-page-url');
+cy.wait(2000); // Wait for 2 seconds
+cy.get('audio').should('be.visible');
+cy.get('audio').should('be.visible').should('have.length', 3).retry(3);
